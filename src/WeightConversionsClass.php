4a5cb077ed00335f53c6ccf8149b63ecb0eb37d8
Anamarija Papic <anamarija.papic@agilo.co>
2023-07-03 14:43:34 +0200

    Add conversion kg to lb

--- src/WeightConversionsClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace AnamarijaPapic\WeightConversions;
-
-class WeightConversionsClass
-{
-}
---
+++ tests/WeightTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use AnamarijaPapic\WeightConversions\Weight;
+
+it('can convert kilograms to pounds', function () {
+    $pounds = Weight::kilograms(100)->toPounds();
+
+    expect($pounds)->toEqual(220.46);
+});
