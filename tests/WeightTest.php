3e215388872a8ec89f8f98243dad5b02aa73ee48
Anamarija Papic <anamarija.papic@agilo.co>
2023-07-03 14:57:01 +0200

    Add conversion kg to st

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -7,3 +7,9 @@ it('can convert kilograms to pounds', function () {
 
     expect($pounds)->toEqual(220.46);
 });
+
+it('can convert kilograms to stones', function () {
+    $pounds = Weight::kilograms(100)->toStones();
+
+    expect($pounds)->toEqual(15.747);
+});
